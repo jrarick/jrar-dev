5e0e7092a0e0cd3534ea86e851c27d1989c130f2
Joshua Rarick <rarick.joshua.e@gmail.com>
2026-01-06 17:06:47 -0600

    updated button styles

---
+++ app/components/button.tsx
@@ -0,0 +1,98 @@
+import {
+  composeRenderProps,
+  Button as RACButton,
+  type ButtonProps as RACButtonProps,
+} from "react-aria-components"
+import { tv } from "tailwind-variants"
+import { focusRing } from "~/lib/utils"
+
+export interface ButtonProps extends RACButtonProps {
+  /** @default 'primary' */
+  variant?: "primary" | "secondary" | "destructive" | "quiet"
+}
+
+let button = tv({
+  extend: focusRing,
+  base: "relative inline-flex items-center justify-center gap-2 border border-transparent h-9 box-border px-3.5 py-0 [&:has(>svg:only-child)]:px-0 [&:has(>svg:only-child)]:h-8 [&:has(>svg:only-child)]:w-8 text-sm text-center cursor-default [-webkit-tap-highlight-color:transparent] transition pressed:scale-97",
+  variants: {
+    variant: {
+      primary:
+        "bg-matrix-accent hover:bg-matrix-vivid text-black shadow-[0_0px_20px_var(--color-matrix-base)]",
+      secondary:
+        "border-matrix-base bg-matrix-muted text-white hover:bg-transparent shadow-[0_0px_20px_var(--color-matrix-base)]",
+      destructive:
+        "bg-ruby-accent hover:bg-ruby-vivid  text-black shadow-[0_0px_20px_var(--color-ruby-base)]",
+      quiet:
+        "border-0 bg-transparent hover:bg-matrix-muted text-white hover:shadow-[0_0px_20px_var(--color-matrix-muted)]",
+    },
+    isDisabled: {
+      true: "border-transparent dark:border-transparent bg-neutral-100 dark:bg-neutral-800 text-neutral-300 dark:text-neutral-600 forced-colors:text-[GrayText]",
+    },
+    isPending: {
+      true: "text-transparent",
+    },
+  },
+  defaultVariants: {
+    variant: "primary",
+  },
+  compoundVariants: [
+    {
+      variant: "quiet",
+      isDisabled: true,
+      class: "bg-transparent dark:bg-transparent",
+    },
+  ],
+})
+
+export function Button(props: ButtonProps) {
+  return (
+    <RACButton
+      {...props}
+      className={composeRenderProps(props.className, (className, renderProps) =>
+        button({ ...renderProps, variant: props.variant, className })
+      )}
+    >
+      {composeRenderProps(props.children, (children, { isPending }) => (
+        <>
+          {children}
+          {isPending && (
+            <span
+              aria-hidden
+              className="flex absolute inset-0 justify-center items-center"
+            >
+              <svg
+                className="w-4 h-4 text-white animate-spin"
+                viewBox="0 0 24 24"
+                stroke={
+                  props.variant === "secondary" || props.variant === "quiet"
+                    ? "light-dark(black, white)"
+                    : "white"
+                }
+              >
+                <circle
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  strokeWidth="4"
+                  fill="none"
+                  className="opacity-25"
+                />
+                <circle
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  strokeWidth="4"
+                  strokeLinecap="round"
+                  fill="none"
+                  pathLength="100"
+                  strokeDasharray="60 140"
+                  strokeDashoffset="0"
+                />
+              </svg>
+            </span>
+          )}
+        </>
+      ))}
+    </RACButton>
+  )
+}
